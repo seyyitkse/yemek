13dcb62a82c1eb1301d1930a2efa6f47d47efea5
Ahmet Seyyit Köse <21110131061@ogr.ksu.edu.tr>
2023-08-01 13:37:26 +0300

    Kullanicinin siparis ekranina gecmesi sağlandi.

--- YemekDunyası/FrmKullaniciGiris.Designer.cs
+++ YemekDunyası/FrmKullaniciGiris.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace YemekDunyası
 {
-    partial class Form1
+    partial class FrmKullaniciGiris
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmKullaniciGiris));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.TxtNick = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.TxtSifre = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.BtnGiris = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
@@ -68,18 +68,18 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(151, 194);
+            this.label2.Location = new System.Drawing.Point(129, 194);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(91, 20);
+            this.label2.Size = new System.Drawing.Size(117, 20);
             this.label2.TabIndex = 2;
-            this.label2.Text = "Kullanıcı ID:";
+            this.label2.Text = "Kullanıcı Adınız:";
             // 
-            // textBox1
+            // TxtNick
             // 
-            this.textBox1.Location = new System.Drawing.Point(246, 191);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(147, 26);
-            this.textBox1.TabIndex = 3;
+            this.TxtNick.Location = new System.Drawing.Point(246, 191);
+            this.TxtNick.Name = "TxtNick";
+            this.TxtNick.Size = new System.Drawing.Size(147, 26);
+            this.TxtNick.TabIndex = 3;
             // 
             // label3
             // 
@@ -91,21 +91,21 @@
             this.label3.TabIndex = 4;
             this.label3.Text = "Kayıtlı mısınız ?";
             // 
-            // textBox2
+            // TxtSifre
             // 
-            this.textBox2.Location = new System.Drawing.Point(246, 223);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(147, 26);
-            this.textBox2.TabIndex = 7;
+            this.TxtSifre.Location = new System.Drawing.Point(246, 223);
+            this.TxtSifre.Name = "TxtSifre";
+            this.TxtSifre.Size = new System.Drawing.Size(147, 26);
+            this.TxtSifre.TabIndex = 7;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(135, 226);
+            this.label5.Location = new System.Drawing.Point(180, 226);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(107, 20);
+            this.label5.Size = new System.Drawing.Size(66, 20);
             this.label5.TabIndex = 6;
-            this.label5.Text = "Kullanıcı Şifre:";
+            this.label5.Text = "Şifreniz:";
             // 
             // BtnGiris
             // 
@@ -115,6 +115,7 @@
             this.BtnGiris.TabIndex = 8;
             this.BtnGiris.Text = "Giriş Yapın";
             this.BtnGiris.UseVisualStyleBackColor = true;
+            this.BtnGiris.Click += new System.EventHandler(this.BtnGiris_Click);
             // 
             // label4
             // 
@@ -150,7 +151,7 @@
             this.pictureBox2.TabStop = false;
             this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
             // 
-            // Form1
+            // FrmKullaniciGiris
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -160,10 +161,10 @@
             this.Controls.Add(this.LinkKayıt);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.BtnGiris);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.TxtSifre);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.TxtNick);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
@@ -171,7 +172,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.Name = "Form1";
+            this.Name = "FrmKullaniciGiris";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "AnaSayfa";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -186,9 +187,9 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox TxtNick;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox TxtSifre;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Button BtnGiris;
         private System.Windows.Forms.Label label4;
---
+++ YemekDunyası/FrmKullaniciGiris.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YemekDunyası
+{
+    public partial class FrmKullaniciGiris : Form
+    {
+        public FrmKullaniciGiris()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+            DialogResult secim= MessageBox.Show("Gerçekten çıkmak mı istiyorsunuz ?", "Uyarı", MessageBoxButtons.OKCancel, MessageBoxIcon.Exclamation);
+            if (secim==DialogResult.OK) 
+            {
+                Application.Exit();
+            }
+            else
+            {
+                MessageBox.Show("Gitmediğinize sevindik :)", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+        }
+
+        private void BtnGiris_Click(object sender, EventArgs e)
+        {
+            if (TxtSifre.Text=="" && TxtNick.Text=="")
+            {
+                MessageBox.Show("Şifre ya da kullanıcı adı boş geçilemez. Lütfen tekrar deneyiniz", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+
+                DbUrunEntity kullanici=new DbUrunEntity();
+
+                var girisYap = from bilgi in kullanici.TBL_MUSTERI where bilgi.KullaniciNick == TxtNick.Text && bilgi.KullaniciSifre == TxtSifre.Text select bilgi;
+                if (girisYap.Any())
+                {
+                    FrmSiparis frmSiparis = new FrmSiparis();
+                    frmSiparis.Show();
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Hatalı şifre ya da ID girdiniz. Lütfen tekrar deneyiniz", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                
+            }
+            
+        }
+    }
+}

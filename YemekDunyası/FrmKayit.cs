c71de359bd1619d140a88949c8a436f43a88d16c
Ahmet Seyyit Köse <21110131061@ogr.ksu.edu.tr>
2023-08-01 15:28:13 +0300

    Musterinin kendini kaydetmesi saglandi.

---
+++ YemekDunyası/FrmKayit.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YemekDunyası
+{
+    public partial class FrmKayit : Form
+    {
+        public FrmKayit()
+        {
+            InitializeComponent();
+        }
+
+
+        FrmKullaniciGiris kullaniciGeriFrm = new FrmKullaniciGiris();
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            DialogResult secim = MessageBox.Show("Gerçekten çıkmak mı istiyorsunuz ?", "Uyarı", MessageBoxButtons.OKCancel, MessageBoxIcon.Exclamation);
+            if (secim == DialogResult.OK)
+            {
+                Application.Exit();
+            }
+            else
+            {
+                kullaniciGeriFrm.Show();
+                this.Hide();
+            }
+        }
+
+
+        DbUrunEntity kullaniciKayit= new DbUrunEntity();
+
+
+        private void BtnKayit_Click(object sender, EventArgs e)
+        {
+            if(TxtAd.Text=="" || TxtSoyad.Text=="" || TxtSifre.Text=="" || TxtNick.Text=="")
+            {
+                MessageBox.Show("Tüm alanları doldurunuz!", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                TBL_MUSTERI yeniMusteri = new TBL_MUSTERI();
+                yeniMusteri.KullaniciAD = TxtAd.Text;
+                yeniMusteri.KullaniciSOYAD = TxtSoyad.Text;
+                yeniMusteri.KullaniciNick = TxtNick.Text;
+                yeniMusteri.KullaniciSifre = TxtSifre.Text;
+                kullaniciKayit.TBL_MUSTERI.Add(yeniMusteri);
+                kullaniciKayit.SaveChanges();
+           
+                DialogResult secim = MessageBox.Show("Kullanıcı kaydedildi. Ana menüye dönmek ister misiniz =", "Uyarı", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+                if (secim == DialogResult.Cancel)
+                {
+                    Application.Exit();
+                }
+                else
+                {
+                    kullaniciGeriFrm.Show();
+                    this.Hide();
+                }
+            }
+        }
+    }
+}
--- YemekDunyası/FrmKullaniciGiris.Designer.cs
+++ YemekDunyası/FrmKullaniciGiris.Designer.cs
@@ -142,6 +142,7 @@
             // 
             // pictureBox2
             // 
+            this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Hand;
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
             this.pictureBox2.Location = new System.Drawing.Point(583, -1);
             this.pictureBox2.Name = "pictureBox2";

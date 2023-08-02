c6187c10fae2e6e28026c58199fccda6263e858b
Ahmet Seyyit Köse <21110131061@ogr.ksu.edu.tr>
2023-08-02 14:57:25 +0300

    Kategori ile ilgili temel islemler(ekleme,silme,guncelleme) islemleri eklendi.

---
+++ YemekDunyası/FrmKategoriIslem.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace YemekDunyası
+{
+    partial class FrmKategoriIslem
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmKategoriIslem));
+            this.BtnAnaSayfa = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.BtnKayit = new System.Windows.Forms.Button();
+            this.TxtID = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.TxtKategoriAd = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.BtnGuncelle = new System.Windows.Forms.Button();
+            this.BtnSil = new System.Windows.Forms.Button();
+            this.BtnListele = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BtnAnaSayfa
+            // 
+            this.BtnAnaSayfa.BackColor = System.Drawing.Color.LightCoral;
+            this.BtnAnaSayfa.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnAnaSayfa.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnAnaSayfa.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BtnAnaSayfa.Location = new System.Drawing.Point(98, 442);
+            this.BtnAnaSayfa.Name = "BtnAnaSayfa";
+            this.BtnAnaSayfa.Size = new System.Drawing.Size(173, 36);
+            this.BtnAnaSayfa.TabIndex = 33;
+            this.BtnAnaSayfa.Text = "Ana Sayfaya Dön";
+            this.BtnAnaSayfa.UseVisualStyleBackColor = false;
+            this.BtnAnaSayfa.Click += new System.EventHandler(this.BtnAnaSayfa_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(141, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(83, 77);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 32;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Comic Sans MS", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.Color.DarkRed;
+            this.label4.Location = new System.Drawing.Point(230, 31);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(323, 40);
+            this.label4.TabIndex = 31;
+            this.label4.Text = "Kategori İşlem Ekranı";
+            // 
+            // BtnKayit
+            // 
+            this.BtnKayit.BackColor = System.Drawing.Color.LightCoral;
+            this.BtnKayit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnKayit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnKayit.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BtnKayit.Location = new System.Drawing.Point(60, 317);
+            this.BtnKayit.Name = "BtnKayit";
+            this.BtnKayit.Size = new System.Drawing.Size(96, 36);
+            this.BtnKayit.TabIndex = 26;
+            this.BtnKayit.Text = "Kaydet";
+            this.BtnKayit.UseVisualStyleBackColor = false;
+            this.BtnKayit.Click += new System.EventHandler(this.BtnKayit_Click);
+            // 
+            // TxtID
+            // 
+            this.TxtID.Location = new System.Drawing.Point(156, 179);
+            this.TxtID.Name = "TxtID";
+            this.TxtID.ReadOnly = true;
+            this.TxtID.Size = new System.Drawing.Size(147, 26);
+            this.TxtID.TabIndex = 25;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(48, 182);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(93, 20);
+            this.label5.TabIndex = 24;
+            this.label5.Text = "Kategori ID:";
+            // 
+            // TxtKategoriAd
+            // 
+            this.TxtKategoriAd.Location = new System.Drawing.Point(156, 216);
+            this.TxtKategoriAd.Name = "TxtKategoriAd";
+            this.TxtKategoriAd.Size = new System.Drawing.Size(147, 26);
+            this.TxtKategoriAd.TabIndex = 23;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 219);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 20);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "Kategori Ad:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(361, 140);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(297, 346);
+            this.dataGridView1.TabIndex = 34;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // BtnGuncelle
+            // 
+            this.BtnGuncelle.BackColor = System.Drawing.Color.LightCoral;
+            this.BtnGuncelle.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnGuncelle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnGuncelle.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BtnGuncelle.Location = new System.Drawing.Point(194, 317);
+            this.BtnGuncelle.Name = "BtnGuncelle";
+            this.BtnGuncelle.Size = new System.Drawing.Size(96, 36);
+            this.BtnGuncelle.TabIndex = 35;
+            this.BtnGuncelle.Text = "Güncelle";
+            this.BtnGuncelle.UseVisualStyleBackColor = false;
+            this.BtnGuncelle.Click += new System.EventHandler(this.BtnGuncelle_Click);
+            // 
+            // BtnSil
+            // 
+            this.BtnSil.BackColor = System.Drawing.Color.LightCoral;
+            this.BtnSil.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnSil.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnSil.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BtnSil.Location = new System.Drawing.Point(60, 375);
+            this.BtnSil.Name = "BtnSil";
+            this.BtnSil.Size = new System.Drawing.Size(96, 36);
+            this.BtnSil.TabIndex = 36;
+            this.BtnSil.Text = "Sil";
+            this.BtnSil.UseVisualStyleBackColor = false;
+            this.BtnSil.Click += new System.EventHandler(this.BtnSil_Click);
+            // 
+            // BtnListele
+            // 
+            this.BtnListele.BackColor = System.Drawing.Color.LightCoral;
+            this.BtnListele.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnListele.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnListele.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BtnListele.Location = new System.Drawing.Point(194, 375);
+            this.BtnListele.Name = "BtnListele";
+            this.BtnListele.Size = new System.Drawing.Size(96, 36);
+            this.BtnListele.TabIndex = 37;
+            this.BtnListele.Text = "Listele";
+            this.BtnListele.UseVisualStyleBackColor = false;
+            this.BtnListele.Click += new System.EventHandler(this.BtnListele_Click);
+            // 
+            // FrmKategoriIslem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(711, 557);
+            this.Controls.Add(this.BtnListele);
+            this.Controls.Add(this.BtnSil);
+            this.Controls.Add(this.BtnGuncelle);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.BtnAnaSayfa);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.BtnKayit);
+            this.Controls.Add(this.TxtID);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.TxtKategoriAd);
+            this.Controls.Add(this.label2);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "FrmKategoriIslem";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FrmKategoriIslem";
+            this.Load += new System.EventHandler(this.FrmKategoriIslem_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnAnaSayfa;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button BtnKayit;
+        private System.Windows.Forms.TextBox TxtID;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox TxtKategoriAd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button BtnGuncelle;
+        private System.Windows.Forms.Button BtnSil;
+        private System.Windows.Forms.Button BtnListele;
+    }
+}
---
+++ YemekDunyası/FrmKategoriIslem.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YemekDunyası
+{
+    public partial class FrmKategoriIslem : Form
+    {
+        public FrmKategoriIslem()
+        {
+            InitializeComponent();
+        }
+        DbUrunEntity kategoriIslem=new DbUrunEntity();
+        
+        public void listele()
+        {
+            var listele = (from bilgi in kategoriIslem.TBL_KATEGORI
+                           select new
+                           {
+                               bilgi.KategoriID,
+                               bilgi.KategoriAD,
+                           }).ToList();
+            dataGridView1.DataSource = listele;
+        }
+
+        private void FrmKategoriIslem_Load(object sender, EventArgs e)
+        {
+            listele();
+        }
+        private void BtnListele_Click(object sender, EventArgs e)
+        {
+            listele();
+        }
+  
+
+        private void BtnKayit_Click(object sender, EventArgs e)
+        {
+            if (TxtKategoriAd.Text == "" || TxtID.Text == "")
+            {
+                MessageBox.Show("Tüm alanları doldurunuz!", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                bool kullaniciAdiVarMi = kategoriIslem.TBL_KATEGORI.Any(k => k.KategoriAD == TxtKategoriAd.Text);
+
+                if (kullaniciAdiVarMi)
+                {
+                    MessageBox.Show("Bu kategori zaten mevcut.Yeni bir kategori eklemeyi deneyin.", "Uyarı", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+                }
+                else
+                {
+                    TBL_KATEGORI yeniKategori = new TBL_KATEGORI();
+                    yeniKategori.KategoriAD = TxtKategoriAd.Text;
+                    kategoriIslem.TBL_KATEGORI.Add(yeniKategori);
+                    kategoriIslem.SaveChanges();
+                    MessageBox.Show("Kategori kaydedildi.", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    listele();
+                }
+            }
+        }
+
+
+        private void BtnGuncelle_Click(object sender, EventArgs e)
+        {
+            if (TxtID.Text=="")
+            {
+                MessageBox.Show("Lütfen güncellemek istediğiniz kategoriyi seçiniz.", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                int kategoriID = Convert.ToInt32(TxtID.Text);
+                var ID = kategoriIslem.TBL_KATEGORI.Find(kategoriID);
+                ID.KategoriAD = TxtKategoriAd.Text;
+                kategoriIslem.SaveChanges();
+                MessageBox.Show("Kategori güncellendi.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                listele();
+            }
+        }
+        private void BtnSil_Click(object sender, EventArgs e)
+        {
+            if (TxtID.Text == "")
+            {
+                MessageBox.Show("Lütfen güncellemek istediğiniz kategoriyi seçiniz.", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                int id = int.Parse(TxtID.Text);
+                var kategoriSil = kategoriIslem.TBL_KATEGORI.Find(id);
+                kategoriIslem.TBL_KATEGORI.Remove(kategoriSil);
+                kategoriIslem.SaveChanges();
+                MessageBox.Show("Kategori silindi.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+        }
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int secilen = dataGridView1.SelectedCells[0].RowIndex;
+            TxtID.Text = dataGridView1.Rows[secilen].Cells[0].Value.ToString();
+            TxtKategoriAd.Text = dataGridView1.Rows[secilen].Cells[1].Value.ToString();
+        }
+
+        private void BtnAnaSayfa_Click(object sender, EventArgs e)
+        {
+            FrmAnaGiris geriFrm= new FrmAnaGiris();
+            geriFrm.Show();
+            this.Hide();
+        }
+    }
+}
